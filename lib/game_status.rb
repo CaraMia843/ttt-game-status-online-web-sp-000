538c881a547a589bc6ba5fdc0f6aa41ad0a73a6c
Cara Zellner <carajeanine@gmail.com>
2020-10-06 21:58:59 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -10,7 +10,7 @@ WIN_COMBINATIONS = [
   [0,4,8], [2,4,6]]
   
 def won?(board)
-  WIN_COMBINATIONS.each {|win_combo|
+  WIN_COMBINATIONS.each do |win_combo|
     index_0 = win_combo[0]
     index_1 = win_combo[1]
     index_2 = win_combo[2]
@@ -24,6 +24,6 @@ def won?(board)
     elsif position_1 == "O" && position_2 == "O" && position_3 == "O"
       return win_combo
     end
-  }
+  end
   return false
 end
