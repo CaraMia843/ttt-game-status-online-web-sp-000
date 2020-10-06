2c96fa3437b4d1e0d133561ed3313cde50db334f
Cara Zellner <carajeanine@gmail.com>
2020-10-06 21:16:42 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -4,4 +4,4 @@ def position_taken?(board, index)
 end
 
 # Define your WIN_COMBINATIONS constant
-WIN_COMBINATIONS = [[0,1,2], [3,4,5], [6,7,8]]
+WIN_COMBINATIONS = [win_combo_top = [0,1,2], [3,4,5], [6,7,8]]
