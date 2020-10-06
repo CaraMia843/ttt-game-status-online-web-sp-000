a593f00eee50725a740cb7a05a40babb1cb37e72
Cara Zellner <carajeanine@gmail.com>
2020-10-06 22:00:14 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -23,7 +23,9 @@ def won?(board)
       return win_combo
     elsif position_1 == "O" && position_2 == "O" && position_3 == "O"
       return win_combo
+    else
+      return false
     end
   end
-  return false
+  
 end
