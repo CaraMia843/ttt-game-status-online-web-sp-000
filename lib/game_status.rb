9b222ec46a7a3c1517caeac401291e6f2e61736b
Cara Zellner <carajeanine@gmail.com>
2020-10-06 22:13:31 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -36,12 +36,10 @@ end
 def draw?(board)
   if !won?(board) && full?(board)
     return true 
-  elsif !won?(board) && !full?(board)
-    return false
-  else won?(board)
+  else
     return false 
   end
-end
+en
 
     
 end
