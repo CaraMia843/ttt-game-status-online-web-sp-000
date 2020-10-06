2a46794d84e3125fefc6c1931e70a450a9a75cf0
Cara Zellner <carajeanine@gmail.com>
2020-10-06 22:21:08 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -5,12 +5,18 @@ end
 
 # Define your WIN_COMBINATIONS constant
 WIN_COMBINATIONS = [
-  [0,1,2], [3,4,5], [6,7,8],
-  [0,3,6], [1,4,7], [2,5,8],
-  [0,4,8], [2,4,6]]
-  
+[0,1,2],
+[3,4,5],
+[6,7,8],
+[0,3,6],
+[1,4,7],
+[2,5,8],
+[0,4,8],
+[6,4,2]
+]
+
 def won?(board)
-  WIN_COMBINATIONS.each do |win_combo|
+  WIN_COMBINATIONS.each {|win_combo|
     index_0 = win_combo[0]
     index_1 = win_combo[1]
     index_2 = win_combo[2]
@@ -24,13 +30,12 @@ def won?(board)
     elsif position_1 == "O" && position_2 == "O" && position_3 == "O"
       return win_combo
     end
-  end
+  }
   return false
 end
 
 def full?(board)
-  board.all? do |index| 
-    index == "X" || index == "O"
+  board.all? {|index| index == "X" || index == "O"}
 end
 
 def draw?(board)
@@ -40,6 +45,28 @@ def draw?(board)
     return false 
   end
 end
+
+def over?(board)
+  if won?(board) || full?(board) || draw?(board)
+    return true 
+  else
+    return false 
+  end
+end
+
+def winner(board)
+  index = []
+  index = won?(board)
+  if index == false 
+    return nil
+  else
+    if board[index[0]] == "X"
+      return "X"
+    else
+      return "O"
+    end
+  end
+end 
 
 
     
